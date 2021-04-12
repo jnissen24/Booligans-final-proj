a9e8c5fe759b740a75a66e7644117514fcf06496
jnissen24 <jacques_nissen@brown.edu>
2021-04-12 19:47:55 -0400

    First display of cards on the screen
    Jacques: Here I created a for loop that places the image "back_of_card" 5 times along the bottom left side of the screen. Took me about an hour due to some technical difficulties

--- speed_display.py
+++ speed_display.py
@@ -5,8 +5,8 @@ margin_left = 250
 margin_top = 150
 
 # window size
-width = 800
-height = 600
+width = 1000
+height = 700
 
 # colors
 black = (0, 0, 0)
@@ -21,13 +21,19 @@ screen = pygame.display.set_mode((width, height))
 screen.fill(gray)
 pygame.display.set_caption("Speed!")
 
-#back_of_card = pygame.image.load('gray_back.png')
+back_of_card = pygame.image.load('./Playing Cards/PNG-cards-1.3/gray_back.png')
 # scale image
-#back_of_card = pygame.transform.scale(back_of_card, (100, 150))
+back_of_card = pygame.transform.scale(back_of_card, (100, 150))
 
 pygame.display.flip()
 running = True
+
+# Right now what this does is put the image "back_of_card" along the bottom left side of the screens
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+    for i in range(0, 5):
+        screen.blit(back_of_card, (100 * i, 550))
+        pygame.display.update()
+    # Not sure exactly what this does right now, but it is needed to avoid an infinite loop and for stuff to appear
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
